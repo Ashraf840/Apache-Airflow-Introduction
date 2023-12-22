9a708aaf427c001176effc6a0be8dad9e004c46e
Ashraf840 <shift2unleashed2011@gmail.com>
2023-12-22 11:36:17 +0600

    Auto store single value returned from a function aka task. Then pull the value to another task via taskInstance.xcomm_pull() method

--- airflow_in_docker/dags/second_dag_with_python_operator.py
+++ airflow_in_docker/dags/second_dag_with_python_operator.py
@@ -9,13 +9,19 @@ default_args = {
 }
 
 
-def greet(name, age):
+# def greet(name, age):
+#     print(f"Hello world!My name is {name} & I'm {age} years old!")
+
+def greet(ti, age):
+    name = ti.xcom_pull(task_ids='get_name')
     print(f"Hello world!My name is {name} & I'm {age} years old!")
 
+def get_name():
+    return 'Jerry'
 
 with DAG(
-    dag_id="dag_with_python_operator_v2",
-    description="Our first dag using python operator! Python function takes parameters!",
+    dag_id="dag_with_python_operator_v4",
+    description="Our first dag using python operator! Python function takes parameters via XCOMS! Push mutliple values through XCOMS!",
     default_args=default_args,
     start_date=datetime(2023, 12, 20, 8),    # from 8am of 20-Dec-2023, so the first execution would be at 21-Dec-2023 at 8am.
     schedule_interval='@daily',
@@ -23,8 +29,14 @@ with DAG(
     task1 = PythonOperator(
         task_id="greet",
         python_callable=greet,
-        op_kwargs={'name': 'Tanjim', 'age': 30},    # Pass a dictionary which will be passes into the python function
+        op_kwargs={'age': 30},    # Pass a dictionary which will be passes into the python function
+    )
+
+    task2 = PythonOperator(
+        task_id="get_name",
+        python_callable=get_name
     )
     
-    task1
+    # task1
+    task2 >> task1
 
